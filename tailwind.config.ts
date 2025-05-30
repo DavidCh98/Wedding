bf4a48f8059b12874ba76b6f2f8d0723e2329e21
DavidCh98 <47038868+DavidCh98@users.noreply.github.com>
2025-05-30 22:02:13 +0200

    New Font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config = {
     },
     extend: {
       fontFamily: {
+         buongiorno: ['"Buongiorno Rastellino"', 'cursive'],
         bellefair: ["var(--font-bellefair)"],
       },
       colors: {
