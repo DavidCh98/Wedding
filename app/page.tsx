e8f863e077bd33b0ebb2794eb9a691e3113ab7fb
ZornittsaI <58049446+ZornittsaI@users.noreply.github.com>
2025-06-15 17:14:14 +0200

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -390,8 +390,8 @@ export default function Home() {
             <div className="mb-12 relative">
               <div className="absolute -inset-6 bg-gradient-to-br from-olive-100 to-olive-50 rounded-2xl transform rotate-3 opacity-50"></div>
               <Image
-                src="/Deyvid + Zori.png"
-                alt="Couple"
+                src="/Deyvid_Zori.png"
+                alt="Couple image"
                 width={600}
                 height={400}
                 className="rounded-2xl shadow-2xl mx-auto relative"
